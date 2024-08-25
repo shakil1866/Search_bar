4ec2180a6ee04689d86fa074bb24f34aa82153fa
SHAKIL AHAMMED <shakilahammed01234@gmail.com>
2024-08-25 20:27:49 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+let searchFun = () => {
+    let filter = document.getElementById("myInput").value.toUpperCase();
+
+    let myTable = document.getElementById("myTable")
+
+    let tr = myTable.getElementsByTagName("tr");
+
+    for (let i = 0; i<tr.length; i++) {
+      let td = tr[i].getElementsByTagName('td')[0];      
+
+    if (td) {
+      let textvlaue = td.textContent || td.innerHTML;
+
+      if (textvlaue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
